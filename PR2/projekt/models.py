06e84d592b45275c9438b37db177d9d1991b7b23
magdabiadala <magdalena.biadala@gmail.com>
2020-01-14 17:50:26 +0100

    Nie działają przyciski

--- PR2/projekt/models.py
+++ PR2/projekt/models.py
@@ -1,17 +1,23 @@
 import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.db import models
 from django.utils import timezone
 
 
 class User(models.Model):
     name = models.CharField('name', max_length=30)
-    surname = models.CharField('surname', max_length=30)
-    date_of_birth = models.DateField('date of birth')
-    login = models.CharField('login', max_length=30)
+    surname = models.CharField('surname', max_length=30, default="surname")
+    date_of_birth = models.DateField('date of birth', default=timezone.now)
+    login = models.CharField('login', max_length=30, default="login")
     is_deleted = models.BooleanField('is_deleted', default=False)
 
     def __str__(self):
         return self.name
 
-    def was_born_recently(self):
-        return self.date_of_birth >= timezone.now() - datetime.timedelta(days=1)
+
+
+'''def delete_user(request, id):
+    User.objects.get(id).delete()
+    return HttpResponseRedirect('/')'''
--- PR2/projekt/views.py
+++ PR2/projekt/views.py
@@ -1,14 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from django.template import loader
 from django.shortcuts import get_object_or_404, render
 
 from .models import User
 
 def index(request):
-    latest_users_list = User.objects.order_by('-date_of_birth')[:5]
-    context = {'latest_users_list': latest_users_list}
+    all_users = User.objects.all().order_by('id')
+    context = {'all_users': all_users}
     return render(request, 'projekt/index.html', context)
 
 def detail(request, id):
     user = get_object_or_404(User, pk=id)
     return render(request, 'projekt/detail.html', {'user': user})
+
+def add_user(request):
+    User(name = request.POST['name']).save()
+    return HttpResponseRedirect('/')
+
+def deleteUser(request, id):
+    User.objects.get(id=id).delete()
+    return HttpResponseRedirect('/')
+
+def comleteUser(request, id):
+    user = User.objects.get(id=id)
+    user.is_deleted = False
+    user.save()
+    return HttpResponseRedirect('/')
